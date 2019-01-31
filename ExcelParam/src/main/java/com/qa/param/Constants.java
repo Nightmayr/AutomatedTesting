c634a4f281042cddee7a0707a6cd3e4a77c50875
Umayr <umayr23@gmail.com>
2019-01-31 10:18:59 +0000

    Added incomplete ExcelParam project

---
+++ ExcelParam/src/main/java/com/qa/param/Constants.java
@@ -0,0 +1,8 @@
+package com.qa.param;
+
+public class Constants {
+	public static final String FILE = "C:\\Users\\Admin\\Desktop\\DemoSiteDDT.xlsx";
+	public static final String ADDUSERPAGE = "http://thedemosite.co.uk/addauser.php";
+	public static final String LOGINPAGE = "http://thedemosite.co.uk/login.php";
+	public final static String DRIVER = "C:\\Users\\Admin\\Applications\\chromedriver.exe";
+}
